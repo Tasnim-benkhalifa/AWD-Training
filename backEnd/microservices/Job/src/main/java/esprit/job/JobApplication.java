b004a59ee558ff916b8efe98503b16742bf96dfd
yassinezemzem <yassine.zemzem@esprit.tn>
2026-02-06 12:05:32 +0100

    jobms

--- backEnd/microservices/Job/src/main/java/esprit/job/JobApplication.java
+++ backEnd/microservices/Job/src/main/java/esprit/job/JobApplication.java
@@ -1,4 +1,4 @@
-package esprit.candidat;
+package esprit.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CandidatApplication {
+public class JobApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CandidatApplication.class, args);
+        SpringApplication.run(JobApplication.class, args);
     }
 
 }
