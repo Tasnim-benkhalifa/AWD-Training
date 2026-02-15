db53741ece8f1ba1d985b8f836d51f1cad02142c
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2026-02-16 00:01:26 +0100

    update workshop

--- frontEnd/JobBoardAngular/src/app/app.module.ts
+++ frontEnd/JobBoardAngular/src/app/app.module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CandidatComponent } from './candidat/candidat.component';
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CandidatComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
